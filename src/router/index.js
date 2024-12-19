73e778642e7e85749a4425df607758fe848520e6
Michail-Gorev <90262826+Michail-Gorev@users.noreply.github.com>
2024-12-20 01:33:29 +0300

    Update index.js
    Исправлены пути до файлов-обёрток

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 
 import { createRouter, createWebHistory } from 'vue-router';
-import App1Wrapper from 'C:/Users/Миша/brain-tests/src/apps/app1/Wrapper.vue';
-import App2Wrapper from 'C:/Users/Миша/brain-tests/src/apps/app2/Wrapper.vue';
-import App3Wrapper from 'C:/Users/Миша/brain-tests/src/apps/app3/Wrapper.vue';
-import App4Wrapper from 'C:/Users/Миша/brain-tests/src/apps/app4/Wrapper.vue';
-import App5Wrapper from 'C:/Users/Миша/brain-tests/src/apps/app5/Wrapper.vue';
+import App1Wrapper from '../apps/app1/Wrapper.vue';
+import App2Wrapper from '../apps/app2/Wrapper.vue';
+import App3Wrapper from '../apps/app3/Wrapper.vue';
+import App4Wrapper from '../apps/app4/Wrapper.vue';
+import App5Wrapper from '../apps/app5/Wrapper.vue';
 
 const routes = [
   { path: '/app1', component: App1Wrapper },
